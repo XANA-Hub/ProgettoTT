6c26a519282ef025070b40191d3f78f085035e14
Alessio Troffei <56554383+alessiotrof@users.noreply.github.com>
2023-04-27 19:17:08 +0200

    Aggiunto Client TCP
    Ora dovrebbe ricevere correttamente dei dati dal Server

---
+++ AppProgettoTT/app/src/main/java/com/example/appprogettott/Client.kt
@@ -0,0 +1,39 @@
+package com.example.appprogettott
+
+import android.os.AsyncTask
+import android.util.Log
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.InetSocketAddress
+import java.net.Socket
+
+class Client: AsyncTask<String?, String?, String?>() {
+
+    override fun doInBackground(vararg params: String?): String? {
+        var socket: Socket? = null
+
+        try {
+            // crea la socket e connettiti al server
+            socket = Socket("192.168.178.149", 25565)
+            Log.d("SERVER_INFO",socket.toString())
+
+
+
+            // crea lo stream di output e invia il messaggio al server
+            //val out = PrintWriter(BufferedWriter(OutputStreamWriter(socket.getOutputStream())), true)
+            //out.println(message)
+
+            // crea lo stream di input e leggi la risposta del server
+            val inStream = BufferedReader(InputStreamReader(socket.getInputStream()))
+            val response = inStream.readLine()
+            Log.d("SUPER_PALLE","Server response: $response")
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+        } finally {
+            // chiudi la socket
+            socket?.close()
+        }
+        return null
+    }
+}
--- AppProgettoTT/app/src/main/java/com/example/appprogettott/MainActivity.kt
+++ AppProgettoTT/app/src/main/java/com/example/appprogettott/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.example.appprogettott
 
+import android.content.Context
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
 import androidx.activity.ComponentActivity
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.Socket
+
+
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart(){
+        super.onStart()
+        val client = Client()
+        client.execute()
 
     }
+
 }
