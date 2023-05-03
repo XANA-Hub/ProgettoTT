b5068cf132f3c27a555e6491df669ca656f0e336
Alessio Troffei <56554383+alessiotrof@users.noreply.github.com>
2023-05-03 19:57:58 +0200

    Aggiunta la socket UDP, forse non funzia

--- AppProgettoTT/app/src/main/java/com/example/appprogettott/Client.kt
+++ AppProgettoTT/app/src/main/java/com/example/appprogettott/Client.kt
@@ -4,33 +4,43 @@ import android.os.AsyncTask
 import android.util.Log
 import java.io.BufferedReader
 import java.io.InputStreamReader
-import java.net.InetSocketAddress
+import java.net.DatagramPacket
+import java.net.DatagramSocket
 import java.net.Socket
 
-class Client: AsyncTask<String?, String?, String?>() {
+class Client : AsyncTask<String?, String?, String?>() {
 
     override fun doInBackground(vararg params: String?): String? {
-        var socket: Socket? = null
+        var socketTcp: Socket? = null
+        var socketUdp: DatagramSocket? = null
 
         try {
-            // crea la socket e connettiti al server
-            socket = Socket("192.168.178.149", 25565)
-            Log.d("SERVER_INFO",socket.toString())
+            // crea la socket TCP e connettiti al server
+            socketTcp = Socket("192.168.178.149", 25565)
 
-            // crea lo stream di output e invia il messaggio al server
-            // val out = PrintWriter(BufferedWriter(OutputStreamWriter(socket.getOutputStream())), true)
-            // out.println(message)
+            // crea lo stream di input e leggi la risposta del server dalla socket TCP
+            val inStream = BufferedReader(InputStreamReader(socketTcp.getInputStream()))
+            val tcpResponse = inStream.readLine()
+            Log.d("SUPER_PALLE_TCP", "TCP response: $tcpResponse")
 
-            // crea lo stream di input e leggi la risposta del server
-            val inStream = BufferedReader(InputStreamReader(socket.getInputStream()))
-            val response = inStream.readLine()
-            Log.d("SUPER_PALLE","Server response: $response")
+
+            // crea la socket UDP
+            socketUdp = DatagramSocket(25564)
+
+            // leggi la risposta dal server sulla socket UDP
+            val receiveBuffer = ByteArray(1024)
+            val receivePacket = DatagramPacket(receiveBuffer, receiveBuffer.size)
+            socketUdp.receive(receivePacket)
+            val udpResponse = String(receivePacket.data, 0, receivePacket.length)
+            Log.d("SUPER_PALLE_UDP", "UDP response: $udpResponse")
+
 
         } catch (e: Exception) {
             e.printStackTrace()
         } finally {
-            // chiudi la socket
-            socket?.close()
+            // chiudi le socket
+            socketTcp?.close()
+            socketUdp?.close()
         }
         return null
     }
