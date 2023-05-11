cefb294f8a71bc86d56724b78a04be6e7904db3f
LucaLombardi <87480145+Lombax99@users.noreply.github.com>
2023-05-11 13:17:48 +0200

    scheletro RobotArm

--- RobotCode/RobotControlArm.py
+++ RobotCode/RobotControlArm.py
@@ -2,8 +2,32 @@ import pykka
 
 class Actor(pykka.ThreadingActor):
     def on_receive(self, message):
-        if "Stop" in message:
+        if "Up" in message:
+            self.ArmUp(message)
+        elif "Down" in message:
+            self.ArmDown(message)
+        elif "Grab" in message:
+            self.Grab(message)
+        elif "Release" in message:
+            self.Release(message)
+        elif "Stop" in message:
             print("--RobotControlArm-- terminating")
             self.stop()
         else:
-            print(message)
+            print("--RobotControlArm ERROR-- malformed message: " + message)
+
+
+    def ArmUp(self, message):
+        print(message)
+
+
+    def ArmDown(self, message):
+        print(message)
+
+
+    def Grab(self, message):
+        print(message)
+        
+
+    def Release(self, message):
+        print(message)
